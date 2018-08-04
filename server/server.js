6f14d0d22effec913b96cd8ffefedbe3e6a9bb7a
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2018-08-04 12:41:27 +0800

    add some mongoose script

--- server/server.js
+++ server/server.js
@@ -7,34 +7,60 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', {
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({
-    text: "Clean my room",
-    completed: false
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    }
 });
 
-newTodo.save()
-    .then(() => {
-        console.log('New todo added');
-    }, e => {
-        console.log('Unable to add new todo', e);
-    })
-
-Todo.create({
-    text: "Buy some foods",
-    completed: true,
-    completedAt: 1234122312,
-}).then(() => {
-    console.log('New todo added');
-}, e => {
-    console.log('Unable to add new todo', e);
+User.create({
+    email: "pramindanata.eksa@gmail.com"
 })
+.then(doc => {
+    console.log('New user added');
+    console.log(doc);
+}, e => {
+    console.log(e);
+})
+
+// let newTodo = new Todo({
+//     text: "      aawdwad",
+//     completed: false
+// });
+
+// newTodo.save()
+//     .then(doc => {
+//         console.log('New todo added');
+//         console.log(doc);
+//     }, e => {
+//         console.log('Unable to add new todo');
+//         console.log(JSON.stringify(e, undefined, 2))
+//     })
+
+// Todo.create({
+//     text: "Buy some foods",
+//     completed: true,
+//     completedAt: 1234122312,
+// }).then(() => {
+//     console.log('New todo added');
+// }, e => {
+//     console.log('Unable to add new todo', e);
+// })
