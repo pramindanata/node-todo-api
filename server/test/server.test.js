67079e51e638add4ae445922ed06d52f37d7ca14
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2018-08-04 16:55:14 +0800

    add test on GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,49 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todos = [
+    {
+        "completed": false,
+        "completedAt": null,
+        "_id": "5b6564693c10dd27c428e511",
+        "text": "Buy some fruits"
+    },
+    {
+        "completed": false,
+        "completedAt": null,
+        "_id": "5b6564723c10dd27c428e512",
+        "text": "Have lunch"
+    },
+    {
+        "completed": false,
+        "completedAt": null,
+        "_id": "5b6564793c10dd27c428e513",
+        "text": "Go outside"
+    }
+];
+
 beforeEach(done => {
     Todo.remove({}).then(() => done());
+});
+
+describe('# GET /todos', () => {
+    it('Should get all todos', done => {
+        Todo.insertMany(todos)
+            .then(() => {
+                request(app)
+                    .get('/todos')
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.body).toEqual({
+                            status: "OK",
+                            data: todos
+                        });
+                    })
+                    .end(done);
+            }, e => {
+                throw e;
+            });
+    });
 });
 
 describe('# POST /todos', () => {
@@ -38,7 +79,7 @@ describe('# POST /todos', () => {
             });
     });
 
-    it('Should not create todo with invalid body data', done => {
+    it('Should not create todo when invalid body data given', done => {
         request(app)
             .post('/todos')
             .send({})
