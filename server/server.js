3942cea9fd3522cb529b5f85201ebb0694cdd167
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2018-08-04 13:29:13 +0800

    add express js and set one POST route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(() => {
+        res.json({
+            status: "OK",
+            message: "New todo added"
+        });
+    }, e => {
+        res.status(400).json({
+            status: "FAILED",
+            message: "Unable to add new todo",
+            content: e
+        })
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
